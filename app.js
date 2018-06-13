ceed5f45662a893ac2704cd6dab95e6e77179a3d
pinklightsaber <ctorres@idemar.cl>
2018-06-12 23:09:58 -0400

    html y js, agregando lat y lng a var y cargandolas

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
+	function initMap() {
+		var hola = new google.maps.LatLng(-34.397,150.644);
+		var cbotado = new google.maps.LatLng(-32.8,-70.55920689999999);
+		var patagallina = new google.maps.LatLng(-38.0587923, -73.22877819999997);
+		var xwlk = new google.maps.LatLng(-31.7477036,-71.51308610000001);
+		var pen = new google.maps.LatLng(-34.78554,-71.043072);
 
-
-	var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {
-          	lat: -34.397, 
-          	lng: 150.644
-          	},
-          zoom: 8
-        });
-      };
+		var map;
+	        map = new google.maps.Map(document.getElementById('map'), {
+	          center: hola,
+	          zoom: 8
+	        });
+	     
 
 
     
 	var cors = 'https://cors-anywhere.herokuapp.com/'; //soy el cors inmortal
 	var url = 'https://api.darksky.net/forecast/';
 	var key = '9e7aa9d7caff4b048f8eb545e8665927/';
-	var coords = { //latitud, longitud
-		cbotado: '-32.8, -70.55920689999999',
-		patagallina: '-38.0587923, -73.22877819999997',
-		xwlk: '-31.7477036,-71.51308610000001',
-		pen:'-34.78554, -71.043072'
-	}
+	
 
 	
+
+	$('#select_lugares').on('change', function(){
+		if ($(this).val()=="cbotado"){
+			map.setCenter(cbotado);
+		} 
+		if ($(this).val()=="pen") { 
+			map.setCenter(pen);
+		}
+		if ($(this).val()=="patagallina") { 
+			map.setCenter(patagallina);
+		}
+		if ($(this).val()=="xwlk") { 
+			map.setCenter(xwlk);
+		}
+	});
+	 };		
